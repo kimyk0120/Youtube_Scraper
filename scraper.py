702bbdf8690296f33a89bc033eb190530e897e59
kimyk0120 <kimyk0120@gmail.com>
2025-01-01 09:58:58 +0900

    mod

--- scraper.py
+++ scraper.py
@@ -3,11 +3,9 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 
-# initialize a web driver instance to control a Chrome window
-# in headless mode
 options = Options()
-# options.add_argument('--headless')
-options.add_argument('--no-sandbox') # 보안 기능 비활성
+# options.add_argument('--headless=new')
+options.add_argument('--no-sandbox')  # 보안 기능 비활성
 options.add_argument("--disable-extensions")  # 확장 프로그램 비활성
 options.add_argument("disable-blink-features=AutomationControlled")  # 자동화 탐지 방지
 options.add_experimental_option("excludeSwitches", ["enable-automation"])  # 자동화 표시 제거
@@ -18,13 +16,23 @@ driver = webdriver.Chrome(
     options=options
 )
 
-# scraping logic...
-url = 'https://www.youtube.com/watch?v=kuDuJWvho7Q'
+def scrape():
+    # scraping logic...
+    url = 'https://www.youtube.com/watch?v=kuDuJWvho7Q'
 
-driver.get(url)
-driver.implicitly_wait(10)
+    driver.get(url)
+    driver.implicitly_wait(10)
 
-print(driver.page_source)
+    # print(driver.page_source)
+    print("test")
 
-# close the browser and free up the resources
-driver.quit()
+    # close the browser and free up the resources
+    driver.quit()
+
+
+if __name__ == "__main__":
+    print("start")
+
+    scrape()
+
+    print("finish")
