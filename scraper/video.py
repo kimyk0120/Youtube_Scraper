c891c7418cb7a2dd0a7ddfb561631c047c124ef2
kimyk0120 <kimyk0120@gmail.com>
2025-01-01 14:06:02 +0900

    mod

--- scraper/video.py
+++ scraper/video.py
@@ -112,6 +112,11 @@ def scrape(url):
             .find_element(By.CSS_SELECTOR, 'like-button-view-model button .yt-spec-button-shape-next__button-text-content') \
             .text
 
+        # TODO scripts
+        driver.find_element(By.CSS_SELECTOR, '#primary .ytd-structured-description-content-renderer .ytd-video-description-transcript-section-renderer button').click()
+
+
+
         video['url'] = url
         video['title'] = title
         video['channel'] = channel
